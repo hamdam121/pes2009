d18aee9240eeade202a079f4d1270979afd6f7e2
Vahid Kheiri <ev121@live.com>
2020-08-26 00:57:31 +0430

    some changes

--- pes2009/Models/SeasonTitle.cs
+++ pes2009/Models/SeasonTitle.cs
@@ -7,11 +7,10 @@ namespace pes2009.Models
 {
     public class SeasonTitle
     {
-        public int ID { get; set; }
+        public int SeasonTitleID { get; set; }
 
-        public virtual int SeasonID { get; set; }
         public virtual int TeamTitleID { get; set; }
-
+        public virtual int SeasonID { get; set; }
         public virtual int TeamID { get; set; }
 
 
